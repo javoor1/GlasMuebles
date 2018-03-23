b2766e35b75ed78842786fb76ae420e09a199191
javoor1 <javoor1@hotmail.com>
2018-03-22 23:48:41 -0600

    first heroku deploy

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module GlassMuebles
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-    config.assets.paths << Rails.root.join("GlassMuebles", "assets", "vendor", "javascripts", "js")
+    config.assets.paths << Rails.root.join("GlassMuebles", "assets", "vendor", "javascripts", "js", "furniture")
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
