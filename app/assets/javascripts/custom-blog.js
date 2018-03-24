40c90aaca8bccad87772bfc25922b1b10567d1f8
javoor1 <javoor1@hotmail.com>
2018-03-24 14:05:26 -0600

    Assets 1000

--- app/assets/javascripts/custom-blog.js
+++ app/assets/javascripts/custom-blog.js
@@ -1,240 +1,240 @@
-(function($) { "use strict";
+// (function($) { "use strict";
 		
 	
-	//Tooltip
+// 	//Tooltip
 	
-	$(".tipped").tipper();	
+// 	$(".tipped").tipper();	
 
 	
- 	//Parallax
+//  	//Parallax
 	
-	$('.parallax-blog').parallax("50%", 0.4);
+// 	$('.parallax-blog').parallax("50%", 0.4);
 		 
 
-	/* Blog Sorting */
+// 	/* Blog Sorting */
 
-	jQuery(document).ready(function($){
-		(function ($) { 
+// 	jQuery(document).ready(function($){
+// 		(function ($) { 
 		
 		
-			var container = $('#blog-grid');
+// 			var container = $('#blog-grid');
 			
 			
-			function getNumbColumns() { 
-				var winWidth = $(window).width(), 
-					columnNumb = 1;
+// 			function getNumbColumns() { 
+// 				var winWidth = $(window).width(), 
+// 					columnNumb = 1;
 				
 				
-				if (winWidth > 1500) {
-					columnNumb = 4;
-				} else if (winWidth > 1200) {
-					columnNumb = 3;
-				} else if (winWidth > 900) {
-					columnNumb = 2;
-				} else if (winWidth > 600) {
-					columnNumb = 2;
-				} else if (winWidth > 300) {
-					columnNumb = 1;
-				}
+// 				if (winWidth > 1500) {
+// 					columnNumb = 4;
+// 				} else if (winWidth > 1200) {
+// 					columnNumb = 3;
+// 				} else if (winWidth > 900) {
+// 					columnNumb = 2;
+// 				} else if (winWidth > 600) {
+// 					columnNumb = 2;
+// 				} else if (winWidth > 300) {
+// 					columnNumb = 1;
+// 				}
 				
-				return columnNumb;
-			}
+// 				return columnNumb;
+// 			}
 			
 			
-			function setColumnWidth() { 
-				var winWidth = $(window).width(), 
-					columnNumb = getNumbColumns(), 
-					postWidth = Math.floor(winWidth / columnNumb);
+// 			function setColumnWidth() { 
+// 				var winWidth = $(window).width(), 
+// 					columnNumb = getNumbColumns(), 
+// 					postWidth = Math.floor(winWidth / columnNumb);
 
-			}
+// 			}
 			
-			$('#blog-filter #filter a').click(function () { 
-				var selector = $(this).attr('data-filter');
+// 			$('#blog-filter #filter a').click(function () { 
+// 				var selector = $(this).attr('data-filter');
 				
-				$(this).parent().parent().find('a').removeClass('current');
-				$(this).addClass('current');
+// 				$(this).parent().parent().find('a').removeClass('current');
+// 				$(this).addClass('current');
 				
-				container.isotope( { 
-					filter : selector 
-				});
+// 				container.isotope( { 
+// 					filter : selector 
+// 				});
 				
-				setTimeout(function () { 
-					reArrangeProjects();
-				}, 300);
+// 				setTimeout(function () { 
+// 					reArrangeProjects();
+// 				}, 300);
 				
 				
-				return false;
-			});
+// 				return false;
+// 			});
 			
-			function reArrangeProjects() { 
-				setColumnWidth();
-				container.isotope('reLayout');
-			}
-			
-			
-			container.imagesLoaded(function () { 
-				setColumnWidth();
-				
-				
-				container.isotope( { 
-					itemSelector : '.blog-box-1', 
-					layoutMode : 'masonry', 
-					resizable : false 
-				} );
-			} );
+// 			function reArrangeProjects() { 
+// 				setColumnWidth();
+// 				container.isotope('reLayout');
+// 			}
 			
 			
-		
+// 			container.imagesLoaded(function () { 
+// 				setColumnWidth();
+				
+				
+// 				container.isotope( { 
+// 					itemSelector : '.blog-box-1', 
+// 					layoutMode : 'masonry', 
+// 					resizable : false 
+// 				} );
+// 			} );
+			
 			
 		
-			$(window).on('debouncedresize', function () { 
-				reArrangeProjects();
-				
-			} );
 			
 		
-		} )(jQuery);
-	} );
+// 			$(window).on('debouncedresize', function () { 
+// 				reArrangeProjects();
+				
+// 			} );
+			
+		
+// 		} )(jQuery);
+// 	} );
 
-	/* DebouncedResize Function */
-		(function ($) { 
-			var $event = $.event, 
-				$special, 
-				resizeTimeout;
+// 	/* DebouncedResize Function */
+// 		(function ($) { 
+// 			var $event = $.event, 
+// 				$special, 
+// 				resizeTimeout;
 			
 			
-			$special = $event.special.debouncedresize = { 
-				setup : function () { 
-					$(this).on('resize', $special.handler);
-				}, 
-				teardown : function () { 
-					$(this).off('resize', $special.handler);
-				}, 
-				handler : function (event, execAsap) { 
-					var context = this, 
-						args = arguments, 
-						dispatch = function () { 
-							event.type = 'debouncedresize';
+// 			$special = $event.special.debouncedresize = { 
+// 				setup : function () { 
+// 					$(this).on('resize', $special.handler);
+// 				}, 
+// 				teardown : function () { 
+// 					$(this).off('resize', $special.handler);
+// 				}, 
+// 				handler : function (event, execAsap) { 
+// 					var context = this, 
+// 						args = arguments, 
+// 						dispatch = function () { 
+// 							event.type = 'debouncedresize';
 							
-							$event.dispatch.apply(context, args);
-						};
+// 							$event.dispatch.apply(context, args);
+// 						};
 					
 					
-					if (resizeTimeout) {
-						clearTimeout(resizeTimeout);
-					}
+// 					if (resizeTimeout) {
+// 						clearTimeout(resizeTimeout);
+// 					}
 					
 					
-					execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
-				}, 
-				threshold : 150 
-			};
-		} )(jQuery);			
+// 					execAsap ? dispatch() : resizeTimeout = setTimeout(dispatch, $special.threshold);
+// 				}, 
+// 				threshold : 150 
+// 			};
+// 		} )(jQuery);			
 
  	
-	/* Blog Carousel */
+// 	/* Blog Carousel */
 	
-	$(document).ready(function() {
+// 	$(document).ready(function() {
 	
-	  var time = 7; // time in seconds
+// 	  var time = 7; // time in seconds
 	 
-	  var $progressBar,
-		  $bar, 
-		  $elem, 
-		  isPause, 
-		  tick,
-		  percentTime;
+// 	  var $progressBar,
+// 		  $bar, 
+// 		  $elem, 
+// 		  isPause, 
+// 		  tick,
+// 		  percentTime;
 	 
-		//Init the carousel
-		$("#owl-blog").owlCarousel({
-			pagination : false,
-			slideSpeed : 500,
-			paginationSpeed : 500,
-			singleItem:true,
-			afterInit : progressBar,
-			afterMove : moved,
-			startDragging : pauseOnDragging			
+// 		//Init the carousel
+// 		$("#owl-blog").owlCarousel({
+// 			pagination : false,
+// 			slideSpeed : 500,
+// 			paginationSpeed : 500,
+// 			singleItem:true,
+// 			afterInit : progressBar,
+// 			afterMove : moved,
+// 			startDragging : pauseOnDragging			
 			
-		});
+// 		});
 
 		
-		//Init progressBar where elem is $("#owl-demo")
-		function progressBar(elem){
-		  $elem = elem;
-		  //build progress bar elements
-		  buildProgressBar();
-		  //start counting
-		  start();
-		}
+// 		//Init progressBar where elem is $("#owl-demo")
+// 		function progressBar(elem){
+// 		  $elem = elem;
+// 		  //build progress bar elements
+// 		  buildProgressBar();
+// 		  //start counting
+// 		  start();
+// 		}
 	 
-		//create div#progressBar and div#bar then prepend to $("#owl-demo")
-		function buildProgressBar(){
-		  $progressBar = $("<div>",{
-			id:"progressBar"
-		  });
-		  $bar = $("<div>",{
-			id:"bar"
-		  });
-		  $progressBar.append($bar).prependTo($elem);
-		}
+// 		//create div#progressBar and div#bar then prepend to $("#owl-demo")
+// 		function buildProgressBar(){
+// 		  $progressBar = $("<div>",{
+// 			id:"progressBar"
+// 		  });
+// 		  $bar = $("<div>",{
+// 			id:"bar"
+// 		  });
+// 		  $progressBar.append($bar).prependTo($elem);
+// 		}
 	 
-		function start() {
-		  //reset timer
-		  percentTime = 0;
-		  isPause = false;
-		  //run interval every 0.01 second
-		  tick = setInterval(interval, 10);
-		};
+// 		function start() {
+// 		  //reset timer
+// 		  percentTime = 0;
+// 		  isPause = false;
+// 		  //run interval every 0.01 second
+// 		  tick = setInterval(interval, 10);
+// 		};
 	 
-		function interval() {
-		  if(isPause === false){
-			percentTime += 1 / time;
-			$bar.css({
-			   width: percentTime+"%"
-			 });
-			//if percentTime is equal or greater than 100
-			if(percentTime >= 100){
-			  //slide to next item 
-			  $elem.trigger('owl.next')
-			}
-		  }
-		}
+// 		function interval() {
+// 		  if(isPause === false){
+// 			percentTime += 1 / time;
+// 			$bar.css({
+// 			   width: percentTime+"%"
+// 			 });
+// 			//if percentTime is equal or greater than 100
+// 			if(percentTime >= 100){
+// 			  //slide to next item 
+// 			  $elem.trigger('owl.next')
+// 			}
+// 		  }
+// 		}
 	 
-		//pause while dragging 
-		function pauseOnDragging(){
-		  isPause = true;
-		}
+// 		//pause while dragging 
+// 		function pauseOnDragging(){
+// 		  isPause = true;
+// 		}
 	 
-		//moved callback
-		function moved(){
-		  //clear interval
-		  clearTimeout(tick);
-		  //start again
-		  start();
-		}
+// 		//moved callback
+// 		function moved(){
+// 		  //clear interval
+// 		  clearTimeout(tick);
+// 		  //start again
+// 		  start();
+// 		}
 	 
-		//uncomment this to make pause on mouseover 
-		// $elem.on('mouseover',function(){
-		//   isPause = true;
-		// })
-		// $elem.on('mouseout',function(){
-		//   isPause = false;
-		// })
+// 		//uncomment this to make pause on mouseover 
+// 		// $elem.on('mouseover',function(){
+// 		//   isPause = true;
+// 		// })
+// 		// $elem.on('mouseout',function(){
+// 		//   isPause = false;
+// 		// })
  	 
-	  var owl = $("#owl-blog");
+// 	  var owl = $("#owl-blog");
 	 
-	  owl.owlCarousel();
+// 	  owl.owlCarousel();
 	 
-	  // Custom Navigation Events
-	  $(".next").click(function(){
-		owl.trigger('owl.next');
-	  })
-	  $(".prev").click(function(){
-		owl.trigger('owl.prev');
-	  })
+// 	  // Custom Navigation Events
+// 	  $(".next").click(function(){
+// 		owl.trigger('owl.next');
+// 	  })
+// 	  $(".prev").click(function(){
+// 		owl.trigger('owl.prev');
+// 	  })
 	 
-	});				
+// 	});				
 
 
 		
-  })(jQuery);
+//   })(jQuery);
